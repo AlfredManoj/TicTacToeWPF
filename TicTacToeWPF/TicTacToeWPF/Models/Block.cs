a0a6d41280d5ab540795b180e6c2c7f3a690b51f
Alfred Manoj <alfredmanj@gmail.com>
2024-06-23 21:47:20 -0400

    added tic tac toe board

---
+++ TicTacToeWPF/TicTacToeWPF/Models/Block.cs
@@ -0,0 +1,33 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicTacToeWPF.Common;
+
+namespace TicTacToeWPF.Models
+{
+    public class Block : PropertyChangedBase
+    {
+        private string? playerIcon;
+        private bool isWinningBlock;
+
+        public int Id { get; set; }
+        public string? PlayerIcon
+        {
+            get => playerIcon;
+            set => Set(ref playerIcon, value);
+        }
+        public bool IsWinningBlock
+        {
+            get => isWinningBlock;
+            set => Set(ref isWinningBlock, value);
+        }
+
+        public void MarkBlock()
+        {
+            PlayerIcon = Players.X.ToString();
+        }
+    }
+}
--- TicTacToeWPF/TicTacToeWPF/ViewModels/MainWindowViewModel.cs
+++ TicTacToeWPF/TicTacToeWPF/ViewModels/MainWindowViewModel.cs
@@ -1,15 +1,36 @@
-﻿namespace TicTacToeWPF
+﻿using Caliburn.Micro;
+using TicTacToeWPF.Common;
+using TicTacToeWPF.Models;
+
+namespace TicTacToeWPF
 {
     /// <summary>
     /// ViewModel class for Mainwindow
     /// </summary>
-    public class MainWindowViewModel
+    public class MainWindowViewModel : Screen
     {
+        private Players currentPlayer;
+        private List<Block> playingBlocks;
+
         /// <summary>
         /// constructor
         /// </summary>
         public MainWindowViewModel()
         {
+            PlayingBlocks = new List<Block>();
+            AddPlayingBlocks();
+        }
+
+        public Players CurrentPlayer
+        {
+            get => currentPlayer;
+            set => Set(ref currentPlayer, value);
+        }
+
+        public List<Block> PlayingBlocks
+        {
+            get => playingBlocks;
+            set => Set(ref playingBlocks, value);
         }
 
         /// <summary>
@@ -24,6 +45,14 @@
         /// </summary>
         public void ResetStatistics()
         {
+        }
+
+        private void AddPlayingBlocks()
+        {
+            for (int blockId = 0; blockId < 9; blockId++)
+            {
+                PlayingBlocks.Add(new Block { Id = blockId });
+            }
         }
     }
 }
